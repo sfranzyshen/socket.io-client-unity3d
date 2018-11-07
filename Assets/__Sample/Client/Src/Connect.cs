f3496d7161ddfd4a4071de11f57770bd1a655ff8
Scotty Franzyshen <sfranzyshen@gmail.com>
2018-11-07 13:05:24 -0800

    Update Connect.cs

--- Assets/__Sample/Client/Src/Connect.cs
+++ Assets/__Sample/Client/Src/Connect.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
 using socket.io;
+using System;
 
 namespace Sample {
 
@@ -9,20 +11,40 @@ namespace Sample {
     public class Connect : MonoBehaviour {
 
         void Start() {
-            var serverUrl = "http://localhost:7001";
+
+            var serverUrl = "http://node0.local:8080";
             var socket = Socket.Connect(serverUrl);
 
+            var myName = 77;
+			
             socket.On(SystemEvents.connect, () => {
-                Debug.Log("Hello, Socket.io~");
+                Debug.Log("Socket.io Connected: ID = " + myName);
+                socket.Emit("connected", myName.ToString() ); // send name
             });
-
+			
+            socket.On("publish", (string data) => {
+                Debug.Log("Socket.io Data: " + data);
+            });
+			
             socket.On(SystemEvents.reconnect, (int reconnectAttempt) => {
-                Debug.Log("Hello, Again! " + reconnectAttempt);
+                Debug.Log("Socket.io Reconnected: Attempt = " + reconnectAttempt);
+                socket.Emit("connected", myName.ToString() ); // send name again ...
             });
 
             socket.On(SystemEvents.disconnect, () => {
-                Debug.Log("Bye~");
+                Debug.Log("Socket.io Disconnected: ID = " + myName);
+				Socket.Reconnect(socket); // reconnect
             });
+
+			socket.On(SystemEvents.connectTimeOut , () => {
+				Debug.Log("Socket.io Connection Timeout: serverUrl = " + serverUrl);
+				Socket.Reconnect(socket); // reconnect
+			});
+
+			socket.On(SystemEvents.connectError , (Exception e) => {
+				Debug.Log("Socket.io Connection Error: " + e.ToString());
+				Socket.Reconnect(socket); // reconnect
+			});
         }
 
     }
